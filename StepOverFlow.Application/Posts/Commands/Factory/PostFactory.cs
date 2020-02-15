b5d9a72acb40fd36cb1b6da2cb15d523f4a4e623
camalzade_elvin <camalzade_elvin@outlook.com>
2020-02-15 07:38:38 -0800

    we add test query and commands

---
+++ StepOverFlow.Application/Posts/Commands/Factory/PostFactory.cs
@@ -0,0 +1,23 @@
+﻿using StepOverFlow.Domain.Posts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StepOverFlow.Application.Posts.Commands.Factory
+{
+    public class PostFactory : IPostFactory
+    {
+        public Post Create(CreatePostModel model)
+        {
+            var post = new Post
+            {
+                Description = model.Description,
+                Title = model.Name,
+                CreatedAt = DateTimeOffset.Now
+            };
+            return post;
+        }
+
+        
+    }
+}
---
+++ StepOverFlow.Application/Posts/Queries/GetPostsList/IGetPostsListQuery.cs
@@ -0,0 +1,12 @@
+﻿using StepOverFlow.Application.Post.Queries.GetPostsList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StepOverFlow.Application.Posts.Queries.GetPostsList
+{
+   public interface IGetPostsListQuery
+    {
+        List<PostListItemModel> Execute();
+    }
+}
